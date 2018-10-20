fb4cf63149c54e1fdb0c71e251913d0e12b83689
milad013 <miladgera014@gmail.com>
2018-10-20 12:53:35 +0330

    otaghak project first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Otaghak Project   ![CI status](https://img.shields.io/badge/build-passing-brightgreen.svg)
+
+
+## Installation
+
+`` git clone https://github.com/milad014/otaghak.git ``
+
+``` install nodeJs ```
+
+`$ npm install -g serve`
+
+
+## Usage
+
+#### for Running
+`$ serve -s otaghak `
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ precache-manifest.fa16384026ee510d54c26aed09b90ff8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "7770a91f0c5da1955f5d",
+    "url": "/static/css/main.1178ae8c.chunk.css"
+  },
+  {
+    "revision": "0239faa05713cdd12ddee309299ae257",
+    "url": "/static/media/Shabnam.0239faa0.eot"
+  },
+  {
+    "revision": "37f16a9fa23d808fa57a",
+    "url": "/static/js/1.37f16a9f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "26cfaa622b7f47648277d7285287f75c",
+    "url": "/static/media/logootaghak.26cfaa62.svg"
+  },
+  {
+    "revision": "50e98ca8e6e26488026a1c525c57000a",
+    "url": "/static/media/imagebox.50e98ca8.jpg"
+  },
+  {
+    "revision": "164bf81963e8ddbd19f9c2c08630a4cc",
+    "url": "/static/media/1.164bf819.svg"
+  },
+  {
+    "revision": "de40d4cc66cdbbb92ed8eb82fee20a83",
+    "url": "/static/media/2.de40d4cc.svg"
+  },
+  {
+    "revision": "bb562ac4b40c8d17620462138a8291a7",
+    "url": "/static/media/3.bb562ac4.svg"
+  },
+  {
+    "revision": "7770a91f0c5da1955f5d",
+    "url": "/static/js/main.7770a91f.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "034ca0f6f1a42508cb52287bd63bd48c",
+    "url": "/static/media/IRANSans.034ca0f6.ttf"
+  },
+  {
+    "revision": "c01f44fd65f0c5cf83cd4fa1ded00566",
+    "url": "/static/media/Shabnam.c01f44fd.woff"
+  },
+  {
+    "revision": "3851778fcf37e19b220258d2a34114ce",
+    "url": "/static/media/Shabnam.3851778f.ttf"
+  },
+  {
+    "revision": "37f16a9fa23d808fa57a",
+    "url": "/static/css/1.2ab8cdb9.chunk.css"
+  },
+  {
+    "revision": "ffc9eba14d236df74a9dfb0290c19ef7",
+    "url": "/index.html"
+  }
+];
